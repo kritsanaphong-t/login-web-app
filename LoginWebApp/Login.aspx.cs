b6a19f1d3f96f2238c8e95df54e4110b44db46c0
Kritsanaphong Tepwerakul <kritsanaphong-keang@hotmail.com>
2024-07-10 06:12:10 +0700

    feat: add session feature

--- LoginWebApp/Login.aspx.cs
+++ LoginWebApp/Login.aspx.cs
@@ -11,15 +11,41 @@ namespace LoginWebApp
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            string username = ReadSession("username");
+            string password = ReadSession("password");
+            CheckLoginInformation(username, password);
         }
 
         protected void LoginButton_Click(object sender, EventArgs e)
         {
-            if (UsernameTextBox.Text == "admin" && PasswordTextBox.Text == "123")
+            string username = UsernameTextBox.Text;
+            string password = PasswordTextBox.Text;
+            WriteSession(username, password);
+            CheckLoginInformation(username, password);
+        }
+
+        private void CheckLoginInformation(string username, string password)
+        {
+            if (username == "admin" && password == "123")
             {
                 Response.Redirect("Welcome.aspx");
             }
+            else
+            {
+                
+            }    
+        }
+
+        private void WriteSession(string username, string password)
+        {
+            Session["username"] = username;
+            Session["password"] = password;
+        }
+
+        private string ReadSession(string key)
+        {
+            if (Session[key] == null) return null;
+            return Session[key].ToString();
         }
     }
 }
